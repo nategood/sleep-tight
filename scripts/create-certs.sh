3c0f632deeb362320199d98c2db0d2c58d4172e9
Alex Pivovarov <alex@revivermx.com>
2014-03-04 16:34:57 -0800

    Validate Server and Client Certificates

--- scripts/create-certs.sh
+++ scripts/create-certs.sh
@@ -18,3 +18,9 @@ openssl req -new -key client.key -out client.csr
 # Sign the client certificate with our CA cert.  Unlike signing our own server cert, this is what we want to do.
 # Serial should be different from the server one, otherwise curl will return NSS error -8054
 openssl x509 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 02 -out client.crt
+
+# Verify Server Certificate
+openssl verify -purpose sslserver -CAfile ca.crt server.crt
+
+# Verify Client Certificate
+openssl verify -purpose sslclient -CAfile ca.crt client.crt
